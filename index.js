e2af8adfd8d126a5538367b3bb02dd0cac01a19a
Lesiba-Tipe <Lesiba-Tipe@users.noreply.github.com>
2021-12-20 16:22:46 +0200

    tipegraphics...

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = 5001
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/about', (req, res) => res.render('pages/about'))
-  .get('/contacts', (req, res) => res.render('pages/contacts'))
-  .get('/potfolio', (req, res) => res.render('pages/potfolio'))
+
+  .get('/data', (req, res) => res.sendFile('./data/data.json', {root:__dirname}))
   .listen(PORT, () => console.log(`Listening on localhost:${ PORT }`))
